7d80b9189b912b42aff6ddebebe614392d6d000b
Voldemar <vezedoes@gmail.com>
2023-04-23 16:03:07 +0300

    Version which works with revenuecat in production

--- App.js
+++ App.js
@@ -1,7 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+
+import Purchases, { LOG_LEVEL } from 'react-native-purchases';
 
 export default function App() {
+  useEffect(() => {
+    try {
+      alert(1)
+      Purchases.setLogLevel(LOG_LEVEL.VERBOSE);
+      alert(2)
+      if (Platform.OS === 'android') {
+        alert(3)
+        Purchases.configure({apiKey: "goog_WONGywBNRxPtGQvyQqcdDkSwXhB"});
+        alert("Success")
+      }
+    } catch (err) {
+      console.log(err);
+      alert(err);
+    }
+  }, [])
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
